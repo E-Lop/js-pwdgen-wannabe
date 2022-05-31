6c2372bbeefc15fab0f50f60d93b39acd363ae95
E-Lop <edmondolopez@gmail.com>
2022-05-31 13:52:52 +0200

    aggiunta richiesta colore

--- js/script.js
+++ js/script.js
@@ -4,3 +4,6 @@ console.log(userName);
 
 const userSurname = prompt('Dimmi il tuo cognome');
 console.log(userSurname);
+
+const favoritecolor = prompt('Dimmi il tuo colore preferito');
+console.log(favoritecolor);
